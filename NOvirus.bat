ea07c8f36eca392e4eb41c9fae352aaa479f5d42
seven42 <50917049+seven42@users.noreply.github.com>
2020-04-19 22:07:57 +0300

    Add files via upload

---
+++ NOvirus.bat
@@ -0,0 +1,12 @@
+@echo off
+del /q /f file2.bat
+del /q /f 2431.bat
+@timeout /t 2 /nobreak >nul 2>nul
+color 02
+:start
+echo %random% %random% %random% %random% WINDOWS ERROR UMBRELLA ATTACK %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% WINDOWS ERROR UMBRELLA ATTACK %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% WINDOWS ERROR UMBRELLA ATTACK %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% WINDOWS ERROR UMBRELLA ATTACK %random% %random% %random% %random% %random%
+@timeout /t 1 /nobreak >nul 2>nul
+goto start
